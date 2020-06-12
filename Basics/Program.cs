f3eb44e5764ddfba37b270200f041c0e5f770361
Evgenii <evs7194@gmail.com>
2020-06-12 13:16:39 +0700

    Literals added

---
+++ Basics/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Literal();
+        }
+
+        static void Varaibles() {
+            // type name
+            string name;
+
+            // set value
+            name = "Tom";
+
+            System.Console.WriteLine(name);
+
+            // initialization during declaration
+            string name1 = "Evgenii";
+            Console.WriteLine(name1);
+        }
+
+        static void Literal() {
+            // logic
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine();
+
+            // integer
+            Console.WriteLine(-11);
+            Console.WriteLine(555);
+            Console.WriteLine();
+
+            // floating point
+            Console.WriteLine(3.14);
+            Console.WriteLine(-273.15);
+            Console.WriteLine();
+
+            // symbol
+            Console.Write('A');
+            Console.Write('\t');
+            Console.WriteLine('t');
+            Console.WriteLine();
+
+            // string
+            Console.WriteLine("Hello world");
+            Console.WriteLine();
+        }
+    }
+}
