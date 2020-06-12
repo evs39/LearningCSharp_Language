2d7e3ed466b6331bdd61985ac2168d9377d0862f
Evgenii <evs7194@gmail.com>
2020-06-12 21:42:37 +0700

    Added data type examples

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -6,10 +6,11 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            Literal();
+            DataTypes();
         }
 
-        static void Varaibles() {
+        static void Varaibles()
+        {
             // type name
             string name;
 
@@ -23,7 +24,8 @@ namespace Basics
             Console.WriteLine(name1);
         }
 
-        static void Literal() {
+        static void Literal()
+        {
             // logic
             Console.WriteLine(true);
             Console.WriteLine(false);
@@ -48,6 +50,69 @@ namespace Basics
             // string
             Console.WriteLine("Hello world");
             Console.WriteLine();
+        }
+
+        static void DataTypes()
+        {
+            bool alive = true; // value type 
+            Console.WriteLine(alive);
+
+            byte bt = 255; // value type
+            sbyte sbt = -128; // value type
+            Console.WriteLine
+            (
+                bt.ToString()
+                + '\t'
+                + sbt.ToString()
+                );
+
+            short s = 30000; // value type
+            ushort us = 60000; // value type
+            Console.WriteLine
+            (
+                s.ToString()
+                + '\t'
+                + us.ToString()
+                );
+
+            int i = -200000000; // value type
+            uint ui = 4000000000; // value type
+            Console.WriteLine
+            (
+                i.ToString()
+                + '\t'
+                + ui.ToString()
+                );
+
+            long l = -3000; // value type
+            ulong ul = 30000; // value type
+            Console.WriteLine
+            (
+                l.ToString()
+                + '\t'
+                + ul.ToString()
+                );
+
+            char c = '\x0420'; // value type
+            string str = "hello"; // reference type
+            Console.WriteLine
+            (
+                c.ToString()
+                + '\t'
+                + str.ToString()
+                );
+
+            float f = 3.123f; // value type
+            double d = 3.134; // value type
+            decimal dcml = 1023.32M; // value type, structure Decimal - floating point number which is able to be involved in financial operations
+            Console.WriteLine
+            (
+                f.ToString()
+                + '\t'
+                + d.ToString()
+                + '\t'
+                + dcml.ToString()
+                );
         }
     }
 }
